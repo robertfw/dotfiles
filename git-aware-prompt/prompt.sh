b0adafffc208ff72195a14f0e8d80a5a04082c7a
Robert <robert.warner@otono.com>
2019-04-15 11:29:56 -0700

    commit modified git-aware-prompt

---
+++ git-aware-prompt/prompt.sh
@@ -0,0 +1,50 @@
+find_git_branch() {
+  # Based on: http://stackoverflow.com/a/13003854/170413
+  local branch
+  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+    if [[ "$branch" == "HEAD" ]]; then
+      branch='detached*'
+    fi
+    git_branch="$branch"
+  else
+    git_branch=""
+  fi
+}
+
+find_git_dirty() {
+  local status=$(git status --porcelain 2> /dev/null)
+  if [[ "$status" != "" ]]; then
+    git_dirty='*'
+  else
+    git_dirty=''
+  fi
+}
+
+last_commit_is_wip() {
+  local status=$(git log -1 --pretty=%B 2> /dev/null)
+  if [[ "$status" == "wip" ]]; then
+    wip_status='WIP'
+  else
+    wip_status=''
+  fi
+}
+
+git_stash_dirty() {
+  local stash=$(git stash list 2> /dev/null)
+  if [[ "$stash" != "" ]]; then
+    git_stash_dirty='STASH'
+  else
+    git_stash_dirty=''
+  fi
+}
+
+PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+
+# Default Git enabled prompt with dirty state
+# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+
+# Another variant:
+# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
+
+# Default Git enabled root prompt (for use with "sudo -s")
+# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
